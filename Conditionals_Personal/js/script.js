4f81ecb37ee2baf2cb56538676994827c1f8aa16
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-19 18:18:16 -0400

    corrections

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,10 +2,14 @@
 // Seasoning for Pork. (need (total seasoning) half of total oz.(Pork)
 
 var Pork= 20  // total oz. of pork
-var salt= 9   // total oz. of salt
-var bPepper= 4 // total oz. black pepper
+var salt= 0  // total oz. of salt
+var bPepper= 10 // total oz. black pepper
 
-var totalS= salt + bPepper // adds the salt and black pepper for total
+var totalS= (salt + bPepper) // adds the salt and black pepper for total in percent
 
+var perPork= (totalS / Pork) // calculates percent.
 
+if perPork <= .5{
+
+}
 
