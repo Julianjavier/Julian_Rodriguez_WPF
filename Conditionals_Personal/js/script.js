f826ace5b9f25f8c48b2c382d3b9830b7d00799c
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-20 17:46:13 -0400

    Condtionals Personal Test2 (no promt)

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,16 +2,16 @@
 // Seasoning for Pork. (need (total seasoning) half of total oz.(Pork)
 
 var Pork= 20  // total oz. of pork
-var salt= 0  // total oz. of salt
-var bPepper= 10 // total oz. black pepper
+var salt= 4  // total oz. of salt
+var bPepper= 5 // total oz. black pepper
 
 var totalS= (salt + bPepper) // adds the salt and black pepper for total in percent
 
 var perPork= (totalS / Pork) // calculates percent.
 
 if (perPork <= .5){
-   "Salt and Pepper are your best friends"
+    console.log ("Salt and Pepper are your best friends")
 } else {
-    "You only got "+totalS+" .oz better get some more"
+    console.log ("You only got "+totalS+" .oz better get some more")
 }
 
