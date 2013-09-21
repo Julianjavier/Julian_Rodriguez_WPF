fd27b989615f5023e0a8667db118e6d4fa1dbe50
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 11:29:00 -0400

    Set Up for new variables

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,2 +1,9 @@
 //Expressions_Industry // Sep 21, 2013
-//PC Hard Drive ports
+//PC 4 port Hard Drive space
+
+var portSpace= [24, 26, 10, 30]
+
+var totalSpace= portSpace [0] + portSpace [1] + portSpace [2] portSpace [3]
+
+
+
