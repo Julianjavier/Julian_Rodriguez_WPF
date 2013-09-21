54cc1bfe184024c877e8b5b8fc75611edd814ef5
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 12:13:44 -0400

    Promt Set up and Test (It Works!)

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,11 +2,11 @@
 //Expressions (Personal) //Sep 21, 2013
 // RPG Defence Calculator
 
-var chest = 12; //chest defence part
-var legs = 23 ; //legs defence part
-var arms= 56  ;  //arms defence part
-var head = 20 ;// head defence part
+var chest = parseInt (prompt("Chest Stat?")); //chest defence part
+var legs = parseInt (prompt("Legs Stat?")) ; //legs defence part
+var arms= parseInt (prompt("Arms Stat?"))  ;  //arms defence part
+var head = parseInt (prompt("Head Stat?")) ;// head defence part
 
-var total= chest+legs+arms+head; //Sets up variable to add all the defence parts.
+var total= (chest+legs+arms+head); //Sets up variable to add all the defence parts.
 
 console.log  (total);
