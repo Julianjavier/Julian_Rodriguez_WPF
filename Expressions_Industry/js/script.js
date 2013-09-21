a44f2bc7d91152c3f8b5665a2d5039297d615ca1
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 03:11:34 -0400

    Code Change( made more simple)

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,9 @@
+//Expressions_Industry
+//Pencil Cuts
+
+var rawStick = 42;
+var numberOfPencil = 3;
+
+var devStick = numberOfPencil % rawStick;
+
+console.log ("You can at least get "+devStick+" equal pencils from this stick")
