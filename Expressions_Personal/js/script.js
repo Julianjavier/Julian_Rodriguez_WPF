fb08bbdb2f111b2f8e76210d104b3905a9b3dc84
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 10:44:38 -0400

    Added Comments

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,12 +1,12 @@
 
-//Expressions (Personal)
+//Expressions (Personal) //Sep 21, 2013
 // RPG Defence Calculator
 
 var chest = 12; //chest defence part
 var legs = 23 ; //legs defence part
 var arms= 56  ;  //arms defence part
-var head = 20 ;// head defence
+var head = 20 ;// head defence part
 
-var total= chest+legs+arms+head;
+var total= chest+legs+arms+head; //Sets up variable to add all the defence parts.
 
 console.log  (total);
