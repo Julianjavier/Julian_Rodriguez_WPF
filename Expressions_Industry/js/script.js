8f0be4e0c567f7555a8e676663aeaf02d2708431
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 12:18:20 -0400

    Setup for Promts and Test (It Worked!)

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,11 +1,14 @@
 //Expressions_Industry // Sep 21, 2013
 //PC 4 port Hard Drive space
 
-var portSpace= [ prompt("Whats the Space on HD1"), prompt("Whats the Space on HD2"), prompt("Whats the Space on HD3"), prompt("Whats the Space on HD4")]
+var portSpace= [ parseInt (prompt("Whats the Space on HD1")),
+                 parseInt (prompt("Whats the Space on HD2")),
+                 parseInt (prompt("Whats the Space on HD3")),
+                 parseInt (prompt("Whats the Space on HD4"))];
 
-var totalSpace= portSpace [0] + portSpace [1] + portSpace [2] + portSpace [3]
+var totalSpace= portSpace [0] + portSpace [1] + portSpace [2] + portSpace [3];
 
-console. log ("You have a total of "+totalSpace+"GB available.")
+console. log ("You have a total of "+totalSpace+"GB available.");
 
 
 
