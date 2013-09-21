b976b0ec3acf2d4f9688a5f8bb0c8d4164b7b513
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 10:20:53 -0400

    Removed Unessesary Variables and Set up Console log

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,10 +1,12 @@
 //Expressions Wacky // Sep 21, 2013
-//Average DPS on Boss Health, every 4 seconds
+//Average DPS on Boss Defence
 
 var ATK= 12
 var bossDef= 14
-var bossHP= 500
+var DPS= 4
 
-var mitigatedDPS= (ATK*4) - bossDef
+var mitigatedDPS= (ATK-bossDef)*DPS
 
-console.log ("You are generating "+mitigatedDPS+" every 4 seconds")
+if (mitigatedDPS <= 0){
+    console.log ("You got gimped gear son!")
+} else console.log ("You are generating "+mitigatedDPS+" every 4 seconds")
