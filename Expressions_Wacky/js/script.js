0a679914ef2499b683b7d97e446a5862a85f76cc
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 10:08:36 -0400

    Small Variable Change

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,7 +1,10 @@
 //Expressions Wacky // Sep 21, 2013
 //Average DPS on Boss Health, every 4 seconds
 
-var DPS= 12
-var bossDef= 5
-var bossHP= 10
+var ATK= 12
+var bossDef= 14
+var bossHP= 500
 
+var mitigatedDPS= (ATK*4) - bossDef
+
+console.log ("You are generating "+mitigatedDPS+" every 4 seconds")
