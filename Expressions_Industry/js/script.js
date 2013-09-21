7a47bbaa9dec7a4a2b93d163443b179434d99e2e
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 12:30:44 -0400

    Added Comments

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,14 +1,14 @@
 //Expressions_Industry // Sep 21, 2013
 //PC 4 port Hard Drive space
 
-var portSpace= [ parseInt (prompt("Whats the Space on HD1")),
+var portSpace= [ parseInt (prompt("Whats the Space on HD1")), // Array that uses different values for the same thing.
                  parseInt (prompt("Whats the Space on HD2")),
                  parseInt (prompt("Whats the Space on HD3")),
                  parseInt (prompt("Whats the Space on HD4"))];
 
-var totalSpace= portSpace [0] + portSpace [1] + portSpace [2] + portSpace [3];
+var totalSpace= portSpace [0] + portSpace [1] + portSpace [2] + portSpace [3];// Adds all the arrays to get a total
 
-console. log ("You have a total of "+totalSpace+"GB available.");
+console. log ("You have a total of "+totalSpace+"GB available.");//shows result on the console.
 
 
 
