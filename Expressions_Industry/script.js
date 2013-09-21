1595da32f84f80a55bd70e1d98687c8c768075a7
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 02:08:16 -0400

    Initial Setup and Variable Setup

---
+++ Expressions_Industry/script.js
@@ -0,0 +1,6 @@
+//Expressions_Industry
+//Pencil Cuts
+
+var rawStick = 30
+var prefferedLen = 10
+var numberOfPencil = 3
---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,12 @@
+
+//Expressions (Personal)
+// RPG Defence Calculator
+
+var chest = 12; //chest defence part
+var legs = 23 ; //legs defence part
+var arms= 56  ;  //arms defence part
+var head = 20 ;// head defence
+
+var total= chest+legs+arms+head;
+
+console.log  (total);
