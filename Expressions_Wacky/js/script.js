d7572e795f075d0d522772db40be3866be83f17e
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 16:34:13 -0400

    Added Coments and move variables to console.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,8 +5,9 @@ var ATK= 12 //attack of a character
 var bossDef= 14 //defence of the boss
 var DPS= 2 //seconds it takes to start attack
 
-var mitigatedDPS= (ATK-bossDef)*DPS
+var mitigatedDPS= (ATK-bossDef)*DPS //calculates for the mitigated attack on the boss
 
+//If statement that makes sure if you are doing damage at all.
 if (mitigatedDPS <= 0){
     console.log ("You got gimped gear son!")
-} else console.log ("You are generating "+mitigatedDPS+" every 4 seconds")
+} else console.log ("You are generating "+mitigatedDPS+" every "+DPS+ "seconds")
