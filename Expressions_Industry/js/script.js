3332886b72665003ed159579b220d77d1b358733
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-21 12:08:13 -0400

    Prompt Setup

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,7 +1,7 @@
 //Expressions_Industry // Sep 21, 2013
 //PC 4 port Hard Drive space
 
-var portSpace= [24, 26, 10, 30]
+var portSpace= [ prompt("Whats the Space on HD1"), prompt("Whats the Space on HD2"), prompt("Whats the Space on HD3"), prompt("Whats the Space on HD4")]
 
 var totalSpace= portSpace [0] + portSpace [1] + portSpace [2] + portSpace [3]
 
