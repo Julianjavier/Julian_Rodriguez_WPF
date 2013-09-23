1fd85988360145689778c9f67b92c6f90058a97a
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-22 22:45:42 -0400

    More Variables and first test

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,6 +2,11 @@
 
 //Cargo Container Balance.
 
-var cargoWeight= 100
-var cargoContainer= 50
-var maxLimit= 500
+var cargoWeight= 300;
+var cargoContainer= 300;
+var maxLimit= 500;
+var totalWeight= cargoWeight+cargoContainer;
+
+if (totalWeight > maxLimit){
+   alert("Sorry you are at "+totalWeight+" tons")
+}
