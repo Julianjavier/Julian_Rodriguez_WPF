045ded7701f8f0ee5ad1350d25fe1b61c5ef8f4e
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-23 17:52:10 -0400

    Consloe log and comments.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -8,5 +8,9 @@ var maxLimit= 500; // Maximum limit for weight.
 var totalWeight= cargoWeight+cargoContainer; //Total weight for the cargo and container.
 
 if (totalWeight > maxLimit){
-   alert("Sorry you are at "+totalWeight+" tons."); // Alerts to notify user it is over the limit.
-} else {alert ("Yes these alert boxes are very necessary.")}; //Alert to notify the user is within limit.
+
+    console.log ("Sorry you are at "+totalWeight+" tons.")//Logs the information in the console browser
+    alert("Sorry you are at "+totalWeight+" tons."); // Alerts to notify user it is over the limit.
+
+} else {console.log ("You are within the limit.")//Logs the information in the console browser
+        alert ("You are within the limit.")}; //Alert to notify the user is within limit.
