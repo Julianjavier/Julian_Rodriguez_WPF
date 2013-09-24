7bafcc5b352e4a04ecff26148b83681be3cca482
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-24 00:10:32 -0400

    Comments added

--- Conditions_Wacky/js/script.js
+++ Conditions_Wacky/js/script.js
@@ -6,8 +6,12 @@ var keyIn2 = prompt("Second Key") //Second key input.
 var rightKey1 = "A" //First correct key input.
 var rightKey2 = "O" //Second correct key input.
 
-if (keyIn1 == rightKey1 && keyIn2 == rightKey2){
+if (keyIn1 == rightKey1 && keyIn2 == rightKey2){// if statement declaring if variables meet conditions to print on the console.
    console. log("Hey, come right in!")
-} else if(keyIn1 != rightKey1 && keyIn2 == rightKey2 || keyIn1 == rightKey1 && keyIn2 != rightKey2){
+}
+//if statement declaring if variables do not meet conditions to print on the console.
+else if(keyIn1 != rightKey1 && keyIn2 == rightKey2 || keyIn1 == rightKey1 && keyIn2 != rightKey2){
    console. log("Close but no cigar.")
-} else ("No! None of that...")
+}
+//Simple else statement to print on console if no conditions are not met.
+else ("No! None of that...")
