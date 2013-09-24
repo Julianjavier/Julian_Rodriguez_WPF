8a3e80fde9cdba37fed18e7b1b2b8134dcbd5e81
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-23 22:23:38 -0400

    Variable set up.

--- Conditions_Wacky/js/script.js
+++ Conditions_Wacky/js/script.js
@@ -1,2 +1,8 @@
 // Dual Digital key lock...thing.  // Sep 23, 2013 // Julian Rodriguez
 
+var keyIn1 = '' //First key input.
+var keyIn2 = '' //Second key input.
+
+var rightKey1 = "A" //First correct key input.
+var rightKey2 = "O" //Second correct key input.
+
