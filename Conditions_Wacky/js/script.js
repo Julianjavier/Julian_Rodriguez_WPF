b4bc43a69861aacfb30ef4aef97181433c94c1c1
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-23 23:15:39 -0400

    Set up for if statment and test.

--- Conditions_Wacky/js/script.js
+++ Conditions_Wacky/js/script.js
@@ -1,8 +1,13 @@
 // Dual Digital key lock...thing.  // Sep 23, 2013 // Julian Rodriguez
 
-var keyIn1 = '' //First key input.
-var keyIn2 = '' //Second key input.
+var keyIn1 = "A" //First key input.
+var keyIn2 = "d" //Second key input.
 
 var rightKey1 = "A" //First correct key input.
 var rightKey2 = "O" //Second correct key input.
 
+if (keyIn1 == rightKey1 && keyIn2 == rightKey2){
+   console. log("Hey, come right in!")
+} else if(keyIn1 != rightKey1 && keyIn2 == rightKey2 || keyIn1 == rightKey1 && keyIn2 != rightKey2){
+   console. log("Close but no cigar.")
+} else ("No! None of that...")
