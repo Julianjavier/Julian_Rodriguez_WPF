9727a97b64cf0bce22beebf1b74da1d8fe265087
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-25 19:39:53 -0400

    Coments, Functions and Flow Chart

---
+++ Functions_Industry/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: julianrodriguez
+ * Date: 9/25/13
+ * Time: 6:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+//Functions Industry
+
+
+function ageAverege( ){      //Function to set up averege age.
+
+}
