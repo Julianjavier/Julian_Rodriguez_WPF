1a85aa890c9797aa84cbddef78890a7d3d34071b
Julian Rodriguez <julianrodriguez@Julian-Rodriguezs-MacBook-Pro.local>
2013-09-25 23:27:28 -0400

    Return message and spellchek

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -10,17 +10,17 @@
 function toDoList( ){ // function to encapsulate the "If" and math.random
    var roulette= Math.random()                                                       //
    var returnMessage= "" ;
-   if (roulette >= .0){ returnMessage= "Do some laundry." }                                         //
-   else if (roulette >= .1){ returnMessage="Do some laundry." }                                    //
-   else if (roulette >= .2){ returnMessage="Make some food." }                                     //
-   else if (roulette >= .3){ returnMessage="Cod some stuff, Yay." }                                //
-   else if (roulette >= .4){ returnMessage="Video Games." }                                        //
-   else if (roulette >= .5){ returnMessage="Do some laundry." }                                    // If statments basing of the math. random
-   else if (roulette >= .6){ returnMessage="Do dishes." }                                          //
-   else if (roulette >= .7){ returnMessage="Forge the Mona Lisa and sell it on the Black Market" } //
-   else if (roulette >= .6){ returnMessage="Do some laundry" }                                     //
-   else if (roulette >= .8){ returnMessage="Cure Cancer" }                                         //
-   else if (roulette >= .9){ returnMessage="Solve World Hunger" }                                  //
+   if (roulette <= .0){ returnMessage= "Do some laundry." }                                        //
+   else if (roulette <= .1){ returnMessage="Do some laundry." }                                    //
+   else if (roulette <= .2){ returnMessage="Make some food." }                                     //
+   else if (roulette <= .3){ returnMessage="Cook some stuff, Yay." }                               //
+   else if (roulette <= .4){ returnMessage="Video Games." }                                        //
+   else if (roulette <= .5){ returnMessage="Do some laundry." }                                    // If statments basing of the math. random
+   else if (roulette <= .6){ returnMessage="Do dishes." }                                          //
+   else if (roulette <= .7){ returnMessage="Forge the Mona Lisa and sell it on the Black Market" } //
+   else if (roulette <= .6){ returnMessage="Do some laundry" }                                     //
+   else if (roulette <= .8){ returnMessage="Cure Cancer" }                                         //
+   else if (roulette <= .9){ returnMessage="Solve World Hunger" }                                  //
    return returnMessage // returns the result the user.
 }
 
